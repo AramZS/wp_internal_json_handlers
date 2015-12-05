5670efb1e60e9b7a88553c66cd4e64e540f1e5de
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-12-05 14:30:43 -0500

    Switch from self:: to $this->

--- json_workers.php
+++ json_workers.php
@@ -2,7 +2,7 @@
 if (!class_exists('ZS_JSON_Workers')){
 	class ZS_JSON_Workers {
 
-		public static $ver = '1.0.0';
+		public static $ver = '1.0.2';
 
 		public static function init() {
             		static $instance;
@@ -24,7 +24,7 @@ if (!class_exists('ZS_JSON_Workers')){
 		}
 
 		public function json_page($json){
-			self::head();
+			$this->head();
 			echo $json;
 		}
 
@@ -37,32 +37,32 @@ if (!class_exists('ZS_JSON_Workers')){
 
 		public function create($args, $error_reporting = false){
 			$json = json_encode($args);
-			return self::handle($json, $error_reporting);
+			return $this->handle($json, $error_reporting);
 		}
 
 		public function create_the($args, $error_reporting = false){
-			echo self::create($args, $error_reporting);
+			echo $this->create($args, $error_reporting);
 			return;
 		}
 
 		public function take($json, $is_array = false, $error_reporting = false){
 			$php_ready = json_decode($json, $is_array);
-			return self::handle($php_ready, $error_reporting);
+			return $this->handle($php_ready, $error_reporting);
 		}
 
 		public function take_the($json, $is_array, $error_reporting){
-			echo self::take($json, $is_array, $error_reporting);
+			echo $this->take($json, $is_array, $error_reporting);
 			return;
 		}
 
 		public function post($url, $args = array(), $settings = array(), $use_get = false, $error_reporting = false){
 			if (!$use_get){
 				if (!empty($settings['method']) && ('POST' != $settings['method'])){
-					return self::post_by_other($url, $args, $settings);
+					return $this->post_by_other($url, $args, $settings);
 				}
-				return self::post_by_post($url, $args, $settings);
+				return $this->post_by_post($url, $args, $settings);
 			} else {
-				return self::post_by_get($url, $args, $settings);
+				return $this->post_by_get($url, $args, $settings);
 			}
 		}
 
@@ -88,34 +88,34 @@ if (!class_exists('ZS_JSON_Workers')){
 				'cookies' => array()
 			);
 			$post_args = wp_parse_args($settings, $default_post_args);
-			$post_args['body'] = self::create($args);
+			$post_args['body'] = $this->create($args);
 			return $post_args;
 		}
 
 		private function post_by_post($url, $args, $settings = array()){
-				$post_args = self::normalize_post_headers($args, $settings);
+				$post_args = $this->normalize_post_headers($args, $settings);
 				$result = wp_remote_post($url, $post_args);
-				$posted = self::post_returned($result, false);
+				$posted = $this->post_returned($result, false);
 				return $posted;
 		}
 
 		private function post_by_other($url, $args = array(), $settings = array()){
-				$request_args = self::normalize_post_headers($args, $settings);
+				$request_args = $this->normalize_post_headers($args, $settings);
 				# wp_safe_remote_request here? https://core.trac.wordpress.org/browser/tags/3.9.2/src/wp-includes/http.php#L0
 				$result = wp_remote_request($url, $request_args);
-				$posted = self::post_returned($result, false);
+				$posted = $this->post_returned($result, false);
 				return $posted;
 		}
 
 		private function post_by_get($url, $args, $settings = array()){
 			$url_complete = add_query_arg($args, $url);
 			$result = wp_remote_get($url_complete, $settings);
-			$posted = self::post_returned($result, false);
+			$posted = $this->post_returned($result, false);
 			return $posted;
 		}
 
 		public function get($url, $args, $settings = array()){
-			return self::post($url, $args, $settings, true);
+			return $this->post($url, $args, $settings, true);
 		}
 	}
 }
